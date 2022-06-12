4f7e3ec860e216fe4c0543441b4a4d37d569f134
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-12 16:07:25 +0200

    update

--- ch02_04_conditionals_start.py
+++ ch02_04_conditionals_start.py
@@ -2,4 +2,13 @@
 # Learning in Tamil - Python 
 # Example file - conditional statements
 #
+def start():
+    x, y = 20, 200
+# How to use if, elif and else 
 
+# How to use conditional statement
+
+# How match-case helps for multiple comparisons
+
+if __name__ == "__main__":
+    start()

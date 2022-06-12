0dc6f671029b11db4052834640121e948a7bcf52
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-12 15:29:18 +0200

    Update ch02_02_variables_completed.py

--- ch02_02_variables_completed.py
+++ ch02_02_variables_completed.py
@@ -6,9 +6,10 @@
 # Basic data types: Numbers, Strings, Booleans, Sequences, Dictionaries
 integerVar = 5
 floatVar = 13.2
+#global String Variable
 stringVar = "This is a string variable"
 booleanVar = True
-list = [0, 1, "two", 3.2, False]
+list = [0, 1, "two", 3.9, True]
 tuple = (0, 1, 2)
 dictionary = {"one" : 1, "two" : 2}
 
@@ -25,10 +26,11 @@ integerVar = "change"
 print (integerVar)
 
 # How to access a member of Sequence
-print(list[2])
-print(tuple[1])
+print(list[1])
+print(tuple[2])
 # How to get the parts of a sequence using slices
-print(list[1:4:2])
+print(list[1:5])
+print(list[1:5:2])
 # How to reverse a sequence using slices
 print(list[::-1])
 
@@ -37,12 +39,12 @@ print(dictionary["one"])
 
 # NOTE: cannot combine variables of different types
 #print ("string type " + 123)
-print ("string type " + str(123))
+print ("name " + str(567))
 
 # Understanding GLOBAL vs local variables in functions
 def newFunction():
     #global stringVar
-    stringVar = "def"
+    stringVar = "newFunction String" #local String Variable
     print (stringVar)
 
 newFunction()
@@ -50,4 +52,3 @@ print (stringVar)
 
 #del stringVar
 print (stringVar)
-

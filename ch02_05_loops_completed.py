0788648e75faa62809a2b65d7910c1c6d2cca0d9
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-12 19:15:09 +0200

    update

--- ch02_05_loops_completed.py
+++ ch02_05_loops_completed.py
@@ -7,29 +7,29 @@ def main():
     x = 0
     
     # while loop
-    while (x < 5):
+    while (x < 8):
         print(x)
         x = x + 1
 
     # for loop
-    for x in range(5,10):
+    for x in range(8,12):
         print (x)
       
     # using for loop with a collection
-    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-    for d in days:
-        print (d)
+    months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    for month in months:
+        print (month)
     
-    # break and continue statements usage
-    for x in range(5,10):
-        #if (x == 7): break
-        #if (x % 2 == 0): continue
+    # break and continue statements usage in loops
+    for x in range(8,12):
+        #if (x == 10): break
+        if (x == 10): continue
         print (x)
     
     # enumerate() function usage to get index 
-    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-    for i, d in enumerate(days):
-        print (i, d)
+    months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    for index, month in enumerate( months):
+        print (index, month)    
   
 if __name__ == "__main__":
     main()

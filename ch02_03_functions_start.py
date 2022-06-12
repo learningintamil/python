07facf43e18f8b9d002b8d75d83ba0a3423d6736
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-12 15:53:57 +0200

    update

--- ch02_03_functions_start.py
+++ ch02_03_functions_start.py
@@ -3,3 +3,12 @@
 # Example file - functions
 #
 
+# new function definition 
+
+# How to use arguments in a function
+
+# How to return a value from a function
+
+# How to give default value to an argument in a function
+
+# How to create a function with variable number of arguments

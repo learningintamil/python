a81322739f0bf7650e907239cac42c1c729a8849
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-12 19:15:58 +0200

    update

--- ch02_07_modules_completed.py
+++ ch02_07_modules_completed.py
@@ -3,13 +3,11 @@
 # Example file - modules
 #
 
-# use mathematics feature --> import math module
+# import math module to use use mathematics
 import math
 
-# many pre-built functions in the math module
-print("The square root of 16 is", math.sqrt(16))
+# math module contains a lot of pre-built functions
+print("Square root of 64 :", math.sqrt(64))
 
-# not only functions, some modules contain useful constants 
-print("Pi is:", math.pi)
-
-# explore other math functions:
+# It has useful constants as well
+print("The value of Pi:", math.pi)

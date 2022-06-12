e29d52deb77975daad6366cde4f82b32223025bd
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-12 19:10:20 +0200

    update

--- ch02_05_loops_start.py
+++ ch02_05_loops_start.py
@@ -2,3 +2,19 @@
 # Learning in Tamil - Python 
 # Example file - loops
 #
+
+def start():
+    x = 0
+
+# How to use 'while loop'
+
+# How to use 'for loop'
+
+# How to use loop with a collection
+
+# Usage of break and continue statements in a loop
+
+# How to get index using enumerate() function 
+
+if __name__ == "__main__":
+    start()

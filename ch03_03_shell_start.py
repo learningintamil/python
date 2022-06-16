e2f17784b34af719e4389da4abbcb6622400c922
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-16 16:09:02 +0200

    update

--- ch03_03_shell_start.py
+++ ch03_03_shell_start.py
@@ -3,3 +3,13 @@
 # Example file - filesystem shell methods
 #
 
+  # How to duplicate an existing file
+
+    # current directory path of the file
+    # append "bak" to the name
+
+    # make a copy using the shutil
+
+    # rename the source file
+
+    # add files to a ZIP archive

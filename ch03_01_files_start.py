dc0c7745bf64b554cd5196430366ac159cdfec7a
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-06-16 16:13:03 +0200

    update

--- ch03_01_files_start.py
+++ ch03_01_files_start.py
@@ -2,3 +2,22 @@
 # Learning in Tamil - Python 
 # Example file - Files
 #
+
+def main():  
+    # Open file --> write data and create new if it doesn't exist
+
+    
+    # Open file --> append text to the end
+
+
+    # write lines of data to the file
+
+    
+    # close the file after completing IO operation
+
+    
+    # Open file --> read the contents
+
+    
+if __name__ == "__main__":
+    main()

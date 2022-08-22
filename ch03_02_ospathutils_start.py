b09ebf83dff4d3dd19c3eab47b8d6f86df417ed9
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-08-22 14:45:35 +0200

    update

--- ch03_02_ospathutils_start.py
+++ ch03_02_ospathutils_start.py
@@ -3,3 +3,22 @@
 # Example file - os.path
 #
 
+import os
+from os import path
+import datetime
+from datetime import date, time, timedelta
+import time
+
+def main():
+    # get the name of the OS
+    
+    # Check for a file existence
+    
+    # file paths
+    
+    # modification time
+    
+    # get file modified date
+    
+if __name__ == "__main__":
+    main()

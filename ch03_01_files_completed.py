0bd746e49402fc4c56550e2e0a954716010d9e00
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-08-22 14:34:54 +0200

    update

--- ch03_01_files_completed.py
+++ ch03_01_files_completed.py
@@ -3,3 +3,31 @@
 # Example file - Files
 #
 
+def main():
+    # Open file --> write data and create new if it doesn't exist
+ 
+    myFile = open("newTextFile.txt","w+")
+    
+    # Open file --> append text to the end
+    # myFile = open("newTextFile.txt","a+")
+
+    # write lines of data to the file
+    for i in range(10):
+        myFile.write("This is appended line number %d\n" % (i+1))
+    
+    # close the file after completing IO operation
+    myFile.close()
+    
+    # Open file --> read the contents
+    myFile = open("newTextFile.txt","r")
+    if myFile.mode == 'r': # to make sure that the file was opened in read mode
+        
+        # fileContents = myFile.read()
+        # print (fileContents)
+      
+        allLines = myFile.readlines() # readlines reads the individual lines into a list
+        for line in allLines:
+            print (line)
+    
+if __name__ == "__main__":
+    main()

74dbbd27b1ab05c880fc50198cd8a7fa72b71db2
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-09-21 15:12:47 +0200

    update

--- ch04_02_formatting_start.py
+++ ch04_02_formatting_start.py
@@ -2,3 +2,18 @@
 # Learning in Tamil - Python 
 # Example file - formatting time and date
 #
+
+from datetime import datetime
+
+def main():
+    print("Formatting date and time")
+    # Get current date and time using datetime
+    
+    # Format the date time
+    
+    # find locale timezone
+    
+    # Format the time
+    
+if __name__ == "__main__":
+    main()

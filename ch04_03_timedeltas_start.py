304ec2c774cdbebe8adbf3ea57da5ceb0ee81fb9
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-09-30 10:24:07 +0200

    update

--- ch04_03_timedeltas_start.py
+++ ch04_03_timedeltas_start.py
@@ -2,3 +2,23 @@
 # Learning in Tamil - Python 
 # Example file - timedelta
 #
+
+from calendar import month
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+# new timedelta object
+
+# current date
+
+# add number of days to current date
+
+#  2 weeks ago
+
+# How many days until Valentine's Day?
+
+#if the day already passed, replace the year with next year by adding 1 to year.
+
+# Valentine's Day  

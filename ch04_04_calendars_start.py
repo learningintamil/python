6a8deafc7bf74e6e917bfcda164eab2743539ddd
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-09-30 18:09:28 +0200

    update

--- ch04_04_calendars_start.py
+++ ch04_04_calendars_start.py
@@ -2,3 +2,15 @@
 # Learning in Tamil - Python 
 # Example file - Calendars
 #
+
+# How to create a plain text calendar
+
+# How to create a HTML calendar
+
+# iterate days of a month
+ 
+# names of days
+
+# names of months
+
+# Calculate project meeting days

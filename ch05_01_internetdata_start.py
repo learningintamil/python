6511b821253adea2d53289d1c7b5c4f2aab24636
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-10-14 11:43:06 +0200

    update

--- ch05_01_internetdata_start.py
+++ ch05_01_internetdata_start.py
@@ -1,4 +1,14 @@
 #
 # Learning in Tamil - Python 
 # Example file - internet
-#
+# 
+
+def main():
+    # open a URL connection 
+    print("internet data")
+    # read HTML Data
+        
+    # response code 
+ 
+if __name__ == "__main__":
+    main()

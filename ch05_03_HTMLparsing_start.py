109402ed896445d18d36e8af4a23d04fcc2cd41c
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-11-07 09:21:50 +0100

    update

--- ch05_03_HTMLparsing_start.py
+++ ch05_03_HTMLparsing_start.py
@@ -2,3 +2,12 @@
 # Learning in Tamil - Python 
 # Example file - HTML
 #
+
+def main():
+    print("HTML Parsing")
+    # Create an instance for the parser and feed the HTML data
+    
+    # open the HTML
+    
+if __name__ == "__main__":
+    main()

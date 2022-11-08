760ae85ee2a84c75db1f9172f1bf238be51de379
LearninginTamil <106994918+learningintamil@users.noreply.github.com>
2022-11-08 09:34:21 +0100

    update

--- ch05_04_XMLparsing_start.py
+++ ch05_04_XMLparsing_start.py
@@ -2,3 +2,11 @@
 # Learning in Tamil - Python 
 # Example file - XML
 #
+
+def main():
+    # load and parse the input XML file using parse function
+    print("XML parsing")
+    
+        
+if __name__ == "__main__":
+    main()
